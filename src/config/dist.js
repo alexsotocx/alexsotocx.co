869857c265c6a718156428f736be9d075eed447d
Alexander Soto <soto@fidor.com>
2017-06-28 16:45:00 +0200

    Fix dev dependencies

--- src/config/dist.js
+++ src/config/dist.js
@@ -4,8 +4,8 @@ import baseConfig from './base';
 
 let config = {
   appEnv: 'dist',
-  blogPostPath: 'http://posts.alexsotocx.co/blogposts/',
-  blogPostsPath: 'http://posts.alexsotocx.co/blogposts/posts.json'
+  blogPostPath: 'http://alexsotocx.github.io/alexsotocx.co/blogposts/',
+  blogPostsPath: 'http://alexsotocx.github.io/alexsotocx.co/blogposts/posts.json'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
